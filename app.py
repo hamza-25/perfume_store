2e9a65fad9de7f95194cdc132187a53d37a8fdf9
hamza-25 <me677499@gmail.com>
2024-01-27 12:08:03 +0100

    update

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html', title='Home Page')
+
+@app.route('/product/<int:id>')
+def single_product(id):
+    return render_template('single_product_page.html', title='product')
+
 if __name__ == '__main__':
     app.run(debug=True)
