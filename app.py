8d57f5d3a9c655630dd6c967be428da1a3f95027
hamza-25 <me677499@gmail.com>
2024-01-29 15:28:16 +0100

    update

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def single_product(id):
     return render_template('single_product_page.html', title='product')
 
+@app.route('/profil')
+def profil():
+    return render_template('profil.html', title='profil')
+
 if __name__ == '__main__':
     app.run(debug=True)
