92b8093f8eb06786189efafb85ec95acc3622829
hamza-25 <me677499@gmail.com>
2024-02-04 12:27:16 +0100

    update

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template
-from db_info import USER, PASSWORD, DATABASE_NAME
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from db_operation import *
+from db_info import USER, PASSWORD, DATABASE_NAME
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisisasecretkeyformyapp'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{USER}:@localhost/{DATABASE_NAME}'
 db = SQLAlchemy(app)
-from models import Category, Product, Address, Order, User
 
+from models import Category, Product, Address, Order, User
+from db_operation import *
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+# login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    from models.User import User
+    return db.session.query(User).filter_by(id=user_id).first()
 
 @app.route('/')
 def home():
@@ -24,10 +37,60 @@ def single_product(id):
     return render_template('single_product_page.html', title='product', product=product)
 
 @app.route('/profil/<int:id>')
+@login_required
 def profil(id):
     from models.User import User
     user = get_profil_by_id(User, db, int(id))
     return render_template('profil.html', title='profil', user=user)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        fname = request.form['first_name']
+        lname = request.form['last_name']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        if password != confirm_password:
+            return 'password not match'
+        # hash_password = generate_password_hash(password)
+        from models.User import User
+        new_user = User(first_name=fname, last_name=lname, password=password, confirm_password=password, email=email, email_verification=email, created_at=datetime.now(), updated_at=datetime.now())
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return render_template("test.html")
+        # return redirect(url_for('home'))
+    return render_template('register.html', title="register page")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        from models.User import User
+
+        # user = User.query.filter_by(email=email).first()  # Query the user from the database by username
+        user = db.session.query(User).filter_by(email=email).first() # Query the user from the database by username
+
+
+        # if user and check_password_hash(user.password, password):  # Check if the user exists and the password is correct
+        #     login_user(user)  # Log in the user using Flask-Login
+        #     return redirect(url_for('home'))  # Redirect to the home page after successful login
+        if user: # Check if the user exists and the password is correct
+            if password == user.password:
+                login_user(user) 
+                return redirect(url_for('home')) 
+            else:
+                return 'Invalid username or password'  # Display error message for invalid credentials
+
+    return render_template('login.html')  # Render the login form
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
--- models/User.py
+++ models/User.py
@@ -1,6 +1,8 @@
 from app import db
+# from datetime import datetime
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(20), nullable=False)
@@ -16,15 +18,17 @@ class User(db.Model):
     orders = db.relationship('Order', backref='user_orders', lazy=True)
 
 
-    # def __init__(self, first_name, last_name, password, confirm_password, email, email_verification, created_at, updated_at):
+    # def __init__(self, first_name, last_name, password, confirm_password, email, email_verification):
     #     self.first_name = first_name
     #     self.last_name = last_name
     #     self.password = password
     #     self.confirm_password = confirm_password
     #     self.email = email
     #     self.email_verification = email_verification
-    #     self.created_at = created_at
-    #     self.updated_at = updated_at
+    #     self.created_at = datetime.now()
+    #     self.updated_at = datetime.now()
+    def __repr__(self):
+        return '<User %r>' % self.username
     
     
     
